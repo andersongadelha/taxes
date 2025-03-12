7eea9ba67ba1fedd9f2896ec8c0247435cd81feb
Anderson Lima <anderson.gadelha@zup.com.br>
2025-03-12 11:40:54 -0300

    feat: add swagger authentication

--- src/main/java/br/com/zup/taxes/infra/SwaggerConfig.java
+++ src/main/java/br/com/zup/taxes/infra/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package br.com.zup.taxes.infra;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,11 +13,19 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        String securitySchemeName = "Bearer Authentication";
         return new OpenAPI()
                 .info(new Info()
                         .title("Taxes API")
                         .version("1.0.0")
-                        .description("Documentação dos endpoints de Taxes API"));
+                        .description("Documentação dos endpoints de Taxes API"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 
 }
